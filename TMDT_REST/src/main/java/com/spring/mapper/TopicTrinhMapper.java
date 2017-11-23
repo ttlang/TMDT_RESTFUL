f173f5f220a675f410a976d8cf0d9114cb38e744
TrinhPham96 <nhoccodaihp96@gmail.com>
2017-11-23 15:48:17 +0700

    Map thử chủ đề

---
+++ TMDT_REST/src/main/java/com/spring/mapper/TopicTrinhMapper.java
@@ -0,0 +1,9 @@
+package com.spring.mapper;
+
+import java.util.List;
+
+import com.spring.domain.Topic_Trinh;
+
+public interface TopicTrinhMapper {
+	public List<Topic_Trinh> getAllTopicsTrinh();
+}
