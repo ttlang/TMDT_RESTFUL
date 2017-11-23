ce14e35fde720b2fa8486c655353acc43f6d96e2
TrinhPham96 <nhoccodaihp96@gmail.com>
2017-11-23 15:53:56 +0700

    Revert "Map thử chủ đề"
    This reverts commit f173f5f220a675f410a976d8cf0d9114cb38e744.

--- TMDT_REST/src/main/java/com/spring/mapper/TopicTrinhMapper.java
+++
@@ -1,9 +0,0 @@
-package com.spring.mapper;
-
-import java.util.List;
-
-import com.spring.domain.Topic_Trinh;
-
-public interface TopicTrinhMapper {
-	public List<Topic_Trinh> getAllTopicsTrinh();
-}
--- TMDT_REST/src/main/java/com/spring/repository/TopicTrinhRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.repository;
-
-import java.util.List;
-
-import com.spring.domain.Topic_Trinh;
-
-public interface TopicTrinhRepository {
-	public List<Topic_Trinh> listTopicsTrinh();
-}
